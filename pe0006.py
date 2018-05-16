b8695fc4a35a9078db4aadf27eed9c313c36d1da
Gareth Funk <gareth.funk@solidspark.co.uk>
2018-05-16 13:11:01 +0100

    Solved problems 6 and 7

---
+++ pe0006.py
@@ -0,0 +1,14 @@
+def sumsquare(n):
+    s = 0
+    for i in range(1, n+1):
+        s += i*i
+    return s
+
+def squaresum(n):
+    s = 0
+    for i in range(1, n+1):
+        s += i
+    s = s*s
+    return s
+
+print(squaresum(100)-sumsquare(100))
---
+++ pe0007.py
@@ -0,0 +1,11 @@
+from pe0003 import isprime
+
+# Let's just run through all the odd natural numbers starting at 3
+primes = [2]
+testnum = 3
+while len(primes) != 10001:
+    if isprime(testnum):
+        primes.append(testnum)
+    testnum += 2
+
+print(primes[-1])
