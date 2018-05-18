38759b2437b3510d870d75f2cc8761bd54dd85b8
Gareth Funk <gareth.funk@solidspark.co.uk>
2018-05-18 17:55:58 +0100

    Solved problem 17

---
+++ pe0017.py
@@ -0,0 +1,8 @@
+# Let's not reinvent the wheel here
+import num2words
+
+s = 0
+for i in range(1, 1001):
+    s += len(num2words.num2words(i, lang='en_GB').replace(" ", "").replace("-", ""))
+
+print(s)
