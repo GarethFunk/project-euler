f403510dc6df04c5609749656ec8967c5ba75c63
Gareth Funk <gareth.funk@solidspark.co.uk>
2018-05-26 21:57:25 +0100

    Solved problem 21

---
+++ pe0021.py
@@ -0,0 +1,30 @@
+from pe0003 import factors
+
+def d(n):
+    # Sum of proper divisors of n
+    s = 0
+    pairs = factors(n)
+    for pair in pairs:
+        if pair[0] == pair[1]:
+            # count divisors only once
+            s += pair[0]
+        elif pair[0] == 1:
+            # Don't add n 
+            s += pair[0]
+        else:
+            s += pair[0]
+            s += pair[1]
+    return s
+
+if __name__ == "__main__":
+    amicables = {}
+    for a in range(1, 10000):
+        if a not in amicables:
+            b = d(a)
+            if b != a and d(b) == a:
+                # These numbers are amicable
+                amicables.update({a:a, b:b})
+    print(sum(amicables.values()))
+            
+    
+    
