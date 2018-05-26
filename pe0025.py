d42caa65377ce1e82121466f757b96a420a7916f
Gareth Funk <gareth.funk@solidspark.co.uk>
2018-05-27 00:01:41 +0100

    Solved problem 25

---
+++ pe0025.py
@@ -0,0 +1,13 @@
+# Let's just generate fibonacci numbers until we find one with more than 1000 digits
+curr = 1
+prev = 1
+i = 2
+
+while len(str(curr)) < 1000:
+    tmp = curr
+    curr = curr + prev
+    prev = tmp
+    i += 1
+
+print(i)
+
