48d649a4ed11eadc305f15d8105f289f8fc6fde5
Gareth Funk <gareth.funk@solidspark.co.uk>
2018-05-26 21:37:01 +0100

    Solved problem 20

---
+++ pe0020.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    retval = 1
+    for i in range(2, n+1): # don't need to multiply by 1
+        retval = retval*i
+    return retval
+
+def sumfactorialdigits(n):
+    num = factorial(n)
+    s = str(num)
+    sumfact = 0
+    for i in range(len(s)):
+        sumfact += int(s[i])
+    return sumfact
+
+if __name__ == "__main__":
+    print(sumfactorialdigits(100))
