31db9e5b9d65e5730eb4161dcbc5d1af4a6d6510
Gareth Funk <gareth.funk@solidspark.co.uk>
2018-05-26 22:14:09 +0100

    Solved problem 22

---
+++ pe0022.py
@@ -0,0 +1,22 @@
+f = open("pe0022_names.txt")
+raw = f.read()
+f.close()
+
+# get rid of quote marks
+raw = raw.replace('"', "")
+names = raw.split(",")
+names.sort()
+
+scores = []
+for i, name in enumerate(names, 1):
+    s = 0
+    name = name.upper()
+    for letter in name:
+        # This adds up the letter as specified in the problem using ascii values
+        s += ord(letter) - 64
+    scores.append(s*i)
+
+print(sum(scores))
+
+
+    
