529bf3dad0aa32e82244740215dc40f3a83142bc
Gareth Funk <gareth.funk@solidspark.co.uk>
2018-05-28 00:51:36 +0100

    Solved problem 29

---
+++ pe0029.py
@@ -0,0 +1,13 @@
+
+def distinctpowers(n):
+    powers = []
+    for a in range(2, n+1):
+        for b in range(2, n+1):
+            powers.append(a**b)
+    return len(set(powers))
+            
+
+
+if __name__ == "__main__":
+    print(distinctpowers(5))
+    print(distinctpowers(100))
