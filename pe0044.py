b4db650d8bb4398b6b6c45cb4b62fa1520a48a48
Gareth Funk <gareth.funk94@gmail.com>
2018-06-08 23:38:19 +0200

    Wrote a function to check if a given number is a pentagon number. MUCH faster than checking new numbers against a list of previously found ones. Also allows code to check numbers not yet seen. Inspired by other solutions seen on the forum.

--- pe0044.py
+++ pe0044.py
@@ -20,39 +20,33 @@ Therfore, the first pair of pentagon numbers we find whose sum and difference ar
 also pentagon numbers will minimise the difference 
 """
 
+from math import sqrt
+
 def p(n):
     return int((n*((3*n) - 1))/2)
 
+def ispentagon(p):
+    # 3n^2 - n - 2p = 0
+    # (1 ± sqrt(1 + 24*p))/6 = n
+    # n > 0 so take +ve square root
+    # 1 + sqrt(1 + 24p) = 6n
+    if (1 + sqrt(1 + (24*p)))%6 == 0:
+        return True
+    else:
+        return False
+
 if __name__ == "__main__":
     pentagons = [p(1)]
-    sums_to_check = []
     n = 2
     current = p(n)
     solved = False
     while not solved:
-        print(n)
-        # The difference between the current pentagonal and the second most recent is a 
-        # a lower bound on the difference between any subsequent pentagonals and those previous
-        # As such we can trim our list of pentagonals
-        cutoff = current - pentagons[-1]
-        for i, pentagon in enumerate(pentagons, 1):
-            if pentagon < cutoff:
-                pentagons.remove(pentagon)
-                continue
+        for pentagon in pentagons:
             difference = current - pentagon
-            # If the difference is a pentagon, we will have already seen it
-            if difference in pentagons:
-                psum = current + pentagon
-                sums_to_check.append((psum, difference, current, pentagon))
-                #print("Difference found: P(" + str(n) + ") - P(" + str(i) + ")")
-        for sums in sums_to_check:
-            if sums[0] in pentagons:
+            psum = current + pentagon
+            if ispentagon(difference) and ispentagon(psum):
+                print((difference, psum, current, pentagon))
                 solved = True
-                print(sums)
-            # We can remove this one from our search if the latest pentagon number is 
-            # larger than it cos pentagons are monotonic increasing. 
-            elif sums[0] < pentagons[-1]:
-                sums_to_check.remove(sums)
         pentagons.append(current)
         n += 1
         current = p(n)
