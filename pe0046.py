2a9ace32579f118a32e68314154a30c301c91a49
Gareth Funk <gareth.funk94@gmail.com>
2018-06-09 00:08:14 +0200

    Solved problem 46.

---
+++ pe0046.py
@@ -0,0 +1,31 @@
+from math import sqrt
+
+from pe0003 import isprime
+
+def isint(n):
+    if int(n) == n:
+        return True
+    else:
+        return False
+
+def goldbach(n, primes):
+    # Primes is every prime number below n
+    for prime in primes:
+        if isint(sqrt((n - prime)/2)):
+            return True
+    return False
+
+if __name__ == "__main__":
+    primes = [2]
+    disproven = False
+    n = 3
+    while disproven is False:
+        if isprime(n):
+            primes.append(n)
+            n += 2  # odd numbers only
+            continue
+        if goldbach(n, primes) is False:
+            disproven = True
+            print(n)
+        n += 2
+        
