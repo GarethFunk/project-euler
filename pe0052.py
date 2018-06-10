3b32ea21bd96919c26e22bbd899115afb6dd1b1a
Gareth Funk <gareth.funk94@gmail.com>
2018-06-10 21:23:12 +0200

    Solved problem 52.

---
+++ pe0052.py
@@ -0,0 +1,18 @@
+
+def getdigits(n):
+    # Returns a sorted list of the digits
+    return sorted(list(str(n)))
+
+if __name__ == "__main__":
+    n = 0
+    found = False
+    while not found:
+        n += 1
+        digits = getdigits(n)
+        for multiplier in [2, 3, 4, 5, 6]:
+            if getdigits(n*multiplier) == digits:
+                found = True
+            else:
+                found = False
+                break
+    print(n)
