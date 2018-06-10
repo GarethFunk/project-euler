6da7c6a4cc8121da2562c8c88fceadd764a05046
Gareth Funk <gareth.funk94@gmail.com>
2018-06-10 22:15:40 +0200

    Solved problem 53.

---
+++ pe0053.py
@@ -0,0 +1,12 @@
+from math import factorial
+
+def nCr (n, r):
+    return factorial(n)//(factorial(r) * factorial(n-r))
+
+if __name__ == "__main__":
+    num_greater_1mil = 0
+    for n in range(1, 101):
+        for r in range(1, n):
+            if nCr(n, r) > 1000000:
+                num_greater_1mil += 1
+    print(num_greater_1mil)
