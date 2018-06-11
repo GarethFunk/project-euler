b3e521fb70cb666e671b20b0f8d0a86f8cb1c524
Gareth Funk <gareth.funk94@gmail.com>
2018-06-11 16:59:12 +0200

    Solved problem 55.

---
+++ pe0055.py
@@ -0,0 +1,18 @@
+from pe0004 import ispallindrome
+
+def reverseadd(n):
+    s = list(str(n))
+    s.reverse()
+    s = "".join(s)
+    return n + int(s)
+
+
+if __name__ == "__main__":
+    num_lychrel = 9999
+    for n in range(1, 10000):
+        for i in range(50):
+            n = reverseadd(n)
+            if ispallindrome(n):
+                num_lychrel -= 1
+                break
+    print(num_lychrel)
