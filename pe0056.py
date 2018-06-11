916c1d42dd7e5914ab2157b8ea539e77eafe12eb
Gareth Funk <gareth.funk94@gmail.com>
2018-06-11 17:05:07 +0200

    Solved problem 56.

---
+++ pe0056.py
@@ -0,0 +1,16 @@
+
+def digitsum(n):
+    s = str(n)
+    digsum = 0
+    for digit in s:
+        digsum += int(digit)
+    return digsum
+
+if __name__ == "__main__":
+    maxdigsum = 0
+    for a in range(1, 100):
+        for b in range(1, 100):
+            digsum = digitsum(a**b)
+            if digsum > maxdigsum:
+                maxdigsum = digsum
+    print(maxdigsum)
